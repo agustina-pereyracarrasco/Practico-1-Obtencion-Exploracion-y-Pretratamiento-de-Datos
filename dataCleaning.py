c74cbffeda596e20daa260a40033bf5a788aac2a
Santiago Martinez <santimartz1995@gmail.com>
2026-03-24 16:11:19 -0300

    primer commit

--- dataCleaning.py
+++ dataCleaning.py
@@ -1,6 +1,26 @@
 import pandas as pd
-print("Pandas cargado correctamente")
-print("Versión:", pd.__version__)
+import matplotlib.pyplot as plt
+from pandas.core.internals.managers import create_block_manager_from_column_arrays  
+
+#-----------Carga de datos-----------#
 
 df = pd.read_csv("Tema_17.csv")
-print(df.head())
+
+
+
+#-----------Cantidad de filas y columnas-----------#
+
+print(df.shape)
+
+
+
+#-----------Imprimir primeras 5 filas-----------#
+
+print(df.head()) 
+
+
+
+#-----------Analisis exploratorio-----------#
+
+
+print(df.dtypes)
