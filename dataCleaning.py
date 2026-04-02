cdd4eeca3b51fd8ea5d356b4d5b2088ca5d5d3f7
Santi <santimartz1995@gmail.com>
2026-04-02 15:22:33 -0300

    Fin analisis exploratorio

--- dataCleaning.py
+++ dataCleaning.py
@@ -28,9 +28,8 @@ print(f"Primeras 5 filas: {df.head()}")
 
 cols = ["CO(GT)", "NMHC(GT)", "C6H6(GT)", "NOx(GT)", "NO2(GT)", "PT08.S5(O3)"]
 df[cols] = df[cols].apply(pd.to_numeric, errors="coerce")
+df = df.replace("invalid_value", -200)
 df = df.replace(-200, pd.NA) #identificar todos los valores faltantes
-
-#print(df.dtypes)
 
 #Calculo de datos faltantes
 faltantes = df.isnull().sum().sum()
@@ -43,25 +42,12 @@ duplicados = df.duplicated().sum()
 print(f"Cantidad de datos duplicados sobre el total: {duplicados}")
 print(f"Porcentaje de datos duplicados sobre el total: {round((duplicados/cantFilas)*100,2)}%")
 
-#----- d) Para los datos faltantes, evaluar posibles motivos de esto en cada caso.-----
-#Teórico
-
-
-#----- Para variables discretas, evaluar los posibles valores de cada variable (valores únicos).-----
-#Teórico
-
-
-
-#----- Cuantificar los valores únicos del punto e) y realizar histogramas.-----
+#----- Cuantificar los valores únicos del punto e) y realizar histogramas.-----#
 
 df['Date'] = pd.to_datetime(df['Date'], format="%d/%m/%Y", errors="coerce")
 df['Time'] = pd.to_datetime(df['Time'], format="%H.%M.%S", errors="coerce").dt.time
 
-
-
 print(df.dtypes)
-
-
 
 print(df.head(100))
 print(df['Date'].nunique())
@@ -69,21 +55,21 @@ print(df["Time"].nunique())
 
 print(df.dtypes)
 
+df["Time_num"] = df["Time"].apply(lambda x: x.hour if pd.notna(x) else None) #nueva columna con las horas representadas de 0 a 23
 
-df["Time_num"] = df["Time"].apply(lambda x: x.hour if pd.notna(x) else None)
-
+#Grafica registros por hora
 df["Time_num"].value_counts().sort_index().plot(kind="bar")
+plt.xlabel("Hora del día (0–23)")
+plt.ylabel("Cantidad de registros")
+plt.title("Distribución de mediciones a lo largo del día")
+plt.xticks(rotation=0)
+plt.grid(axis="y")
 
-plt.xlabel("Hora")
-plt.ylabel("Frecuencia")
-plt.title("Cantidad de registros por hora")
-plt.xticks(rotation=45)
 plt.show()
 
 
 #-----Check inconsistencia en los datos
 print("INICIO CHECK INCONSISTENCIA")
-
 
 #Temperatura
 
@@ -112,31 +98,65 @@ df[cols] = df[cols].apply(pd.to_numeric, errors="coerce")
 for col in cols:
     print(col, (df[col] < 0).sum()) #Valores negativos
     print(df[col].describe()) #muestra estadisticas generales de cada columna (cantidad de filas, min, max, promedio..)
-    
 
+#Valores limites
+
+co_inconsistente = (df["CO(GT)"] < 0) | (df["CO(GT)"] > 80)    
+print("Valores limites de CO: ",co_inconsistente.sum())
+
+nmhc_inconsistente = ((df['NMHC(GT)'] > 1000)|((df['NMHC(GT)']) < 0)).sum()
+print("Valores limites de NMHC", nmhc_inconsistente)
+
+#print(df["NMHC(GT)"].isna().mean()) usar en limpieza (muchos valores NaN)
+
+c6h6_inconsistente = ((df['C6H6(GT)'] > 200)|(df['C6H6(GT)'] < 0)).sum()
+print("Valores limites de C6H6", c6h6_inconsistente)
+
+nox_inconsistente = (df["NOx(GT)"] < 0) | (df["NOx(GT)"] > 1000)
+print("Valores limites nox: ",nox_inconsistente.sum())
+
+no2_inconsistente = (df["NO2(GT)"] < 0) | (df["NO2(GT)"] > 500)
+print("Valores limites no2: ",no2_inconsistente.sum())
 
 df[cols].isna().sum() #Valores faltantes
 
-conteo = (df['PT08.S5(O3)'] > 1374).sum()
-print("Valores limites de ozono: ", conteo)
+conteoOzono = (df['PT08.S5(O3)'] > 1374).sum()
+print("Valores limites de ozono: ", conteoOzono)
 correctos = df['PT08.S5(O3)'].count()
 print("Valores no nulos de ozono: ",correctos)
- 
-df[cols].apply(pd.to_numeric, errors="coerce").hist(bins=30)
+
+
+#Graficas
+
+
+df[cols].apply(pd.to_numeric, errors="coerce").hist(bins=30, figsize=(12,8))
+plt.suptitle("Distribución de contaminantes", fontsize=16)
 plt.show()
 
+
+plt.figure(figsize=(12,6))
 df.boxplot(column=cols)
+plt.title("Detección de outliers en contaminantes")
+plt.xlabel("Contaminantes")
+plt.ylabel("Concentración")
 plt.xticks(rotation=45)
+plt.grid(True)
 plt.show()
 
-df.groupby("Time_num")["CO(GT)"].mean().plot(kind="bar")
 
-plt.xlabel("Hora del día")
-plt.ylabel("CO promedio")
-plt.title("Concentración promedio de CO por hora")
-
-plt.xticks(rotation=0)  # opcional
-plt.show()
+for col in cols:
+    plt.figure()
+    
+    df.groupby("Time_num")[col].mean().plot(kind="bar")
+    
+    plt.xlabel("Hora del día (0–23)")
+    plt.ylabel(f"Concentración promedio de {col}")
+    plt.title(f"Variación diaria de {col}")
+    
+    plt.xticks(rotation=0)
+    plt.grid(axis="y")
+    
+    plt.show()
 
 
 
